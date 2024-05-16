12bcd8d53900d849e2f86a95b1de413277218afd
qhardwick <qhardwick@gmail.com>
2024-05-16 17:49:23 -0400

    unit tests for taxreturn service class

---
+++ src/test/java/com/skillstorm/taxservice/services/TaxReturnServiceTest.java
@@ -0,0 +1,166 @@
+package com.skillstorm.taxservice.services;
+
+import com.skillstorm.taxservice.exceptions.NotFoundException;
+import com.skillstorm.taxservice.models.TaxReturn;
+import com.skillstorm.taxservice.repositories.TaxReturnRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TaxReturnServiceTest {
+
+    @InjectMocks private static TaxReturnService taxReturnService;
+
+    @Mock private TaxReturnRepository taxReturnRepository;
+
+    private static TaxReturn newTaxReturn;
+    private static TaxReturn returnedNewTaxReturn;
+    private static TaxReturn updatedTaxReturn;
+
+    @BeforeEach
+    void setUp() {
+        taxReturnService = new TaxReturnService(taxReturnRepository);
+
+        setupTaxReturns();
+    }
+
+    private void setupTaxReturns() {
+        newTaxReturn = new TaxReturn();
+        newTaxReturn.setYear(2024);
+        newTaxReturn.setUserId(1);
+
+        returnedNewTaxReturn = new TaxReturn();
+        returnedNewTaxReturn.setId(1);
+        returnedNewTaxReturn.setYear(2024);
+        returnedNewTaxReturn.setUserId(1);
+
+        updatedTaxReturn = new TaxReturn();
+        updatedTaxReturn.setId(1);
+        updatedTaxReturn.setYear(2024);
+        updatedTaxReturn.setUserId(1);
+        updatedTaxReturn.setFirstName("TestFirstName");
+        updatedTaxReturn.setLastName("TestLastName");
+        updatedTaxReturn.setAddress("TestAddress");
+        updatedTaxReturn.setCity("TestCity");
+        updatedTaxReturn.setState("TestState");
+        updatedTaxReturn.setZip("TestZipCode");
+        updatedTaxReturn.setRefund(BigDecimal.ZERO.setScale(2));
+    }
+
+    // Add new TaxReturn:
+    @Test
+    void addTaxReturn() {
+
+        // Define stubbing:
+        when(taxReturnRepository.saveAndFlush(newTaxReturn)).thenReturn(returnedNewTaxReturn);
+
+        // Call the method to be tested:
+        TaxReturn result = taxReturnService.addTaxReturn(newTaxReturn);
+
+        // Verify the result:
+        assertEquals(1, result.getId(), "The TaxReturn ID should be 1.");
+        assertEquals(2024, result.getYear(), "The TaxReturn year should be 2024.");
+        assertEquals(1, result.getUserId(), "The TaxReturn user ID should be 1.");
+    }
+
+    // Get TaxReturn by id success:
+    @Test
+    void findByIdSuccess() {
+
+        // Define stubbing:
+        when(taxReturnRepository.findById(1)).thenReturn(java.util.Optional.of(returnedNewTaxReturn));
+
+        // Call the method to be tested:
+        TaxReturn result = taxReturnService.findById(1);
+
+        // Verify the result:
+        assertEquals(1, result.getId(), "The TaxReturn ID should be 1.");
+        assertEquals(2024, result.getYear(), "The TaxReturn year should be 2024.");
+        assertEquals(1, result.getUserId(), "The TaxReturn user ID should be 1.");
+    }
+
+    // Get TaxReturn by id failure:
+    @Test
+    void findByIdFailure() {
+
+        // Define stubbing:
+        when(taxReturnRepository.findById(1)).thenReturn(java.util.Optional.empty());
+
+        // Verify the exception
+        assertThrows(NotFoundException.class, () -> taxReturnService.findById(1), "NotFoundException should be thrown.");
+    }
+
+    // Find all TaxReturns by userId:
+    @Test
+    void findAllByUserId() {
+
+        // Define stubbing:
+        when(taxReturnRepository.findAllByUserId(1)).thenReturn(List.of(returnedNewTaxReturn));
+
+        // Call the method to be tested:
+        List<TaxReturn> result = taxReturnService.findAllByUserId(1);
+
+        // Verify the result:
+        assertEquals(1, result.size(), "The size of the list should be 1.");
+        assertEquals(1, result.get(0).getId(), "The TaxReturn ID should be 1.");
+        assertEquals(2024, result.get(0).getYear(), "The TaxReturn year should be 2024.");
+        assertEquals(1, result.get(0).getUserId(), "The TaxReturn user ID should be 1.");
+    }
+
+    // Update TaxReturn:
+    @Test
+    void updateTaxReturn() {
+
+        // Define stubbing:
+        when(taxReturnRepository.findById(1)).thenReturn(java.util.Optional.of(returnedNewTaxReturn));
+        when(taxReturnRepository.saveAndFlush(updatedTaxReturn)).thenReturn(updatedTaxReturn);
+
+        // Call the method to be tested:
+        TaxReturn result = taxReturnService.updateTaxReturn(1, updatedTaxReturn);
+
+        // Verify the result:
+        assertEquals(1, result.getId(), "The TaxReturn ID should be 1.");
+        assertEquals(2024, result.getYear(), "The TaxReturn year should be 2024.");
+        assertEquals(1, result.getUserId(), "The TaxReturn user ID should be 1.");
+        assertEquals("TestFirstName", result.getFirstName(), "The TaxReturn first name should be TestFirstName.");
+        assertEquals("TestLastName", result.getLastName(), "The TaxReturn last name should be TestLastName.");
+        assertEquals("TestAddress", result.getAddress(), "The TaxReturn address should be TestAddress.");
+        assertEquals("TestCity", result.getCity(), "The TaxReturn city should be TestCity.");
+        assertEquals("TestState", result.getState(), "The TaxReturn state should be TestState.");
+        assertEquals("TestZipCode", result.getZip(), "The TaxReturn zip code should be TestZipCode.");
+        assertEquals(BigDecimal.ZERO.setScale(2), result.getRefund(), "The TaxReturn refund should be 0.00.");
+    }
+
+    // Delete TaxReturn:
+    @Test
+    void deleteTaxReturn() {
+
+        // Define stubbing:
+        when(taxReturnRepository.findById(1)).thenReturn(java.util.Optional.of(updatedTaxReturn));
+
+        //Define ArgumentCaptor:
+        ArgumentCaptor<Integer> idCaptor = ArgumentCaptor.forClass(Integer.class);
+
+        // Call the method to be tested:
+        taxReturnService.deleteTaxReturn(1);
+
+        // Capture the argument passed to the deleteById method
+        verify(taxReturnRepository).deleteById(idCaptor.capture());
+
+        // Verify the result:
+        assertEquals(1, idCaptor.getValue(), "The TaxReturn ID should be 1.");
+    }
+}
