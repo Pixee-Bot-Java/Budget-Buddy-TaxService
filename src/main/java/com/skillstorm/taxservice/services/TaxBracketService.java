8ec28f370bd6d51a333fcf0ff6d3fb2c56acc9ca
monsieurCat <falharbi.m.96@gmail.com>
2024-05-22 14:04:39 -0700

    use constructor injection for TaxBracketService; add tests

--- src/main/java/com/skillstorm/taxservice/services/TaxBracketService.java
+++ src/main/java/com/skillstorm/taxservice/services/TaxBracketService.java
@@ -2,7 +2,6 @@ package com.skillstorm.taxservice.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skillstorm.taxservice.models.TaxBracket;
@@ -11,8 +10,11 @@ import com.skillstorm.taxservice.repositories.TaxBracketRepository;
 @Service
 public class TaxBracketService {
   
-  @Autowired
-  TaxBracketRepository taxBracketRepository;
+  private final TaxBracketRepository taxBracketRepository;
+
+  public TaxBracketService(TaxBracketRepository taxBracketRepository) {
+    this.taxBracketRepository = taxBracketRepository;
+  }
 
   // Get the tax bracket by filing status id
   public List<TaxBracket> findByFilingStatusID(int id) {
