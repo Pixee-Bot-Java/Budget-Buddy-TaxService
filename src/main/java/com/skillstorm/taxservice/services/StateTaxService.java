127c74d47755e91d3a657423063d5c68781c9794
monsieurCat <falharbi.m.96@gmail.com>
2024-05-22 14:01:20 -0700

    use constructor injection for StateTaxService; add tests

--- src/main/java/com/skillstorm/taxservice/services/StateTaxService.java
+++ src/main/java/com/skillstorm/taxservice/services/StateTaxService.java
@@ -2,7 +2,6 @@ package com.skillstorm.taxservice.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skillstorm.taxservice.exceptions.NotFoundException;
@@ -12,8 +11,11 @@ import com.skillstorm.taxservice.repositories.StateTaxRepository;
 @Service
 public class StateTaxService {
   
-  @Autowired
-  private StateTaxRepository stateTaxRepository;
+  private final StateTaxRepository stateTaxRepository;
+
+  public StateTaxService(StateTaxRepository stateTaxRepository) {
+    this.stateTaxRepository = stateTaxRepository;
+  }
 
   // Get state income tax brackets by state name
   public List<StateTax> getTaxBracketsByStateName(String stateName) {
@@ -41,5 +43,4 @@ public class StateTaxService {
     }
     return taxBrackets;
   }
-
 }
